f1563a7e3d1ff1c1fcb609746aa4e5bb87d90790
cw75 <cgwu0530@gmail.com>
2019-12-20 01:10:44 +0000

    fix test

--- tests/shared/test_serializer.py
+++ tests/shared/test_serializer.py
@@ -104,7 +104,7 @@ class TestSerializer(unittest.TestCase):
         inpt = {'a': 1, 'b': 2}
         serialized = self.serializer.dump_lattice(inpt)
 
-        self.assertEqual(type(serialized), MapLattice)
+        self.assertEqual(type(serialized), LWWPairLattice)
         output = self.serializer.load_lattice(serialized)
 
         self.assertEqual(output, inpt)
